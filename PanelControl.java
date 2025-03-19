683d5ac20774487cc15a817524a2c68559dc1b16
arelyUACM <arely.olivares@estudiante.uacm.edu.mx>
2025-03-19 17:24:10 -0600

    2do Commit

--- PanelControl.java
+++ PanelControl.java
@@ -1,6 +1,11 @@
 public interface PanelControl {
 
+    // Método existente
     public void eventoA();
 
+    // Método existente
     public void eventoB();
+
+    // Nuevo método para prueba
+    public void eventoC();  // Cambio de prueba
 }
