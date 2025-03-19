9c921309b00726f35afff1f500516e2cbb46f0fa
Enrique222666 <enrique.delarosa.ramos@estudiante.uacm.edu.mx>
2025-03-19 17:13:36 -0600

    se crea main

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        ControladorB controlador = new ControladorB();
-        
-        // Probando los métodos del controlador
-        controlador.eventoA();
-        controlador.eventoB();
+
     }
 }
