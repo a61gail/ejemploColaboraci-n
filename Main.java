d553db5c7f264e9340678c8b9fdbe17890caabd8
Angel <angelgp755@gmail.com>
2025-03-26 17:00:13 -0600

    Se uso el controladorB en Main

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@
  public class Main {
     public static void main(String[] args) {
 
+        ControladorA controlA= new ControladorA();
+
+        controlA.eventoA();
+        controlA.eventoB();
+
     }
 }
